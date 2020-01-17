c4f98855e4897a888055c2d565bc30ef445560f6
Pujan Poudel <pujan@syntegrate.com.np>
2020-01-17 17:17:01 +0545

    Convert xls and xlxs to json. CSV not included for while

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ### Express for web server
 ### Multer for file upload
 ### Body Parser to parse the post data
+### xls-to-json-lc for converting .xls to json
+### xlsx-to-json-lc for converting .xlsx to json
+
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var xlstojson = require("xls-to-json-lc");
+var xlsxtojson = require("xlsx-to-json-lc");
 app.use(bodyParser.json());
 
 //Disk Storage configuration for multer
@@ -20,7 +22,7 @@ var upload = multer({
     storage: storage,
     //Filter to accept excel and csv file
     fileFilter: function(req, file, callback) {
-        if (['xls', 'xlsx', 'csv'].indexOf(file.originalname.split('.')[file.originalname.split('.').length - 1]) === -1) {
+        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length - 1]) === -1) {
             return callback(new Error('Wrong extension type. Only xls,xlsx and csv are accepted.').message);
         }
         callback(null, true);
@@ -35,7 +37,35 @@ app.post('/upload', function(req, res) {
             res.json({ error_code: 1, err_desc: err });
             return;
         }
-        res.json({ error_code: 0, err_desc: null });
+        /** Check if the file is stored or not by multer*/
+        if (!req.file) {
+            res.json({ error_code: 1, err_desc: "No file Found" })
+        }
+
+        //Start Conversion
+        /** Check extension of the file 
+         * Use appropriate module to convert the file
+         */
+        if (req.file.originalname.split('.')[req.file.originalname.split('.').length - 1] === 'xlsx') {
+            exceltojson = xlsxtojson;
+        } else {
+            exceltojson = xlstojson;
+        }
+
+        try {
+            exceltojson({
+                input: req.file.path, //the same path where we uploaded our file
+                output: null, //since we don't need output.json
+                lowerCaseHeaders: true
+            }, function(err, result) {
+                if (err) {
+                    return res.json({ error_code: 1, err_desc: err, data: null });
+                }
+                res.json({ error_code: 0, err_desc: null, data: result });
+            });
+        } catch (e) {
+            res.json({ error_code: 1, err_desc: "Corupted excel file" });
+        }
     })
 });
 
